4a82ce4c43af1272c94c6b5f820cab4913cc3042
xamatR <77069479+xamatR@users.noreply.github.com>
2021-06-27 17:11:23 -0300

    ultimo comit terminando o projeto

--- app/src/main/java/br/ufjf/dcc196/matheus/calculadora/MainActivity.java
+++ app/src/main/java/br/ufjf/dcc196/matheus/calculadora/MainActivity.java
@@ -3,31 +3,64 @@ package br.ufjf.dcc196.matheus.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private EditText valor1;
     private EditText valor2;
     private TextView resultado;
+    private RadioGroup grupoOperacao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        valor1 = findViewById(R.id.valor1);
+        valor2 = findViewById(R.id.valor2);
+        resultado = findViewById(R.id.Resultado);
+        grupoOperacao = findViewById(R.id.grupoOperacao);
     }
     public void calcula(View view){
-        valor1 = findViewById(R.id.valor1);
         Double v1 = Double.parseDouble(valor1.getText().toString());
-        valor2 = findViewById(R.id.valor2);
         Double v2= Double.parseDouble(valor2.getText().toString());
+        Double r=0.0;
         // calcula
-        resultado = findViewById(R.id.Resultado);
-        Double r= v1+v2;
-        resultado.setText(r.toString());
+        switch (grupoOperacao.getCheckedRadioButtonId()){
+                case R.id.Soma:
+                    r = v1+v2;
+                    resultado.setText(r.toString());
+                    valor1.setText(r.toString());
+                    break;
+                case R.id.subtracao:
+                    r= v1-v2;
+                    resultado.setText(r.toString());
+                    valor1.setText(r.toString());
+                    break;
+                case R.id.multiplicacao:
+                    r= v1*v2;
+                    resultado.setText(r.toString());
+                    valor1.setText(r.toString());
+                    break;
+                case R.id.divisao:
+                    try {
+                        r= v1/v2;
+                        resultado.setText(r.toString());
+                        valor1.setText(r.toString());
+                    }catch (ArithmeticException e) {
+                        resultado.setText("Divisão por 0 invalida.");
+
+                    }
+                    break;
+                default:
+                    resultado.setText("Digite dois numeros.");
+            }
 
 
-    }
+        }
+
 
 }
 
